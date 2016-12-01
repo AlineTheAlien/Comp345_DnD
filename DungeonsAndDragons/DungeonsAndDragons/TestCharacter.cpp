2d42c20759fded43f126306e50c3ad9a8d51a861
Aline <aline.koftikian@gmail.com>
2016-11-30 20:21:33 -0500

    Added character builder test cases

--- DungeonsAndDragons/DungeonsAndDragons/TestCharacter.cpp
+++ DungeonsAndDragons/DungeonsAndDragons/TestCharacter.cpp
@@ -17,10 +17,9 @@
 #include <cppunit/CompilerOutputter.h>
 #include <cppunit/XmlOutputter.h>
 #include "Character.h"
-#include "MapObject.h"
-#include "ItemContainer.h"
-#include "Armor.h"
-#include "Weapon.h"
+#include "BullyBuilder.h"
+#include "TankBuilder.h"
+#include "Director.h"
 #include <stdio.h>
 #include <math.h> 
 using namespace CppUnit;
@@ -32,15 +31,51 @@ class TestCharacter : public CppUnit::TestFixture
 	CPPUNIT_TEST_SUITE(TestCharacter);
 	CPPUNIT_TEST(testEquipItem);
 	CPPUNIT_TEST(testUnequipItem);
+	CPPUNIT_TEST(testBuilderCreation);
+	CPPUNIT_TEST(testNumberOfAttacks);
 	CPPUNIT_TEST_SUITE_END();
 protected:
 	void testAggressorStrategy();
 	void testEquipItem();
 	void testUnequipItem();
+	void testBuilderCreation();
+	void testNumberOfAttacks();
 };
 
 //!Register for running the test
 CPPUNIT_TEST_SUITE_REGISTRATION(TestCharacter);//most important
+
+
+//! Test case to ensure that the builder is correctly assigning ability scores
+//! Test Case: A properly build character should have all of its ability scores in the [3,18] interval
+//! Tested item: Director::constructCharacter()
+void TestCharacter::testBuilderCreation() {
+	Director director;
+	Character* myCharacter;
+	CharacterBuilder* bullybuilder = new BullyBuilder();
+	director.setCharacterBuilder(bullybuilder);
+	director.constructCharacter();
+	myCharacter = director.getCharacter();
+	CPPUNIT_ASSERT(myCharacter->validateNewCharacter());
+}
+
+//! Test case to ensure that the number of attacks/round increases by one every 5 levels
+//! Test Case: A correct implementation of numofattacks/round is if it increases by one per 5 levels
+//! Tested item: Character::levelUp()
+void TestCharacter::testNumberOfAttacks() {
+	Director director;
+	Character* myCharacter;
+	CharacterBuilder* tankbuilder = new TankBuilder();
+	director.setCharacterBuilder(tankbuilder);
+	director.constructCharacter();
+	myCharacter = director.getCharacter();
+
+	for (int i = 0; i < 21; i++) {
+		myCharacter->levelUp();
+	}
+	CPPUNIT_ASSERT(myCharacter->getNumberOfAttacks() == 5);
+}
+
 
 //! Test method to test character equip an item
 //! Test Case: When character equips an item, the size of 'equipped' array should increase, and the size of 'backpack' should decrease
